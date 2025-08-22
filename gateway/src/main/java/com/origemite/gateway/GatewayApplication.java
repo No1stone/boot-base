a4aee26cfeb61c5c7494318f6d5fbf7d82cf6500
jangwonseok <jangws1003@naver.com>
2025-08-22 23:26:05 +0900

    [gateway][feat]: 그래들임포트

--- gateway/src/main/java/com/origemite/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/origemite/gateway/GatewayApplication.java
@@ -1,9 +1,16 @@
 package com.origemite.gateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {
+        "com.origemite.gateway",
+        "io.origemite.lib.webflux",
+}
+)
+@OpenAPIDefinition
 public class GatewayApplication {
 
     public static void main(String[] args) {
