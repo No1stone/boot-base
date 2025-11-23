3e910ca61f3661d3a2cc1def7b8a34916a844d31
jangwonseok <jangws1003@naver.com>
2025-11-23 16:16:31 +0900

    [gateway][feat]: 파이명..

--- gateway/src/main/java/com/origemite/gateway/filter/ServerAllowPath.java
+++ gateway/src/main/java/com/origemite/gateway/filter/ServerAllowPath.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class CodeshopServerAllowPath {
+public class ServerAllowPath {
 
     public static final Set<String> ALLOWED_PATHS = new HashSet<>(Arrays.asList(
             "/auth/api/v1/token/**",
