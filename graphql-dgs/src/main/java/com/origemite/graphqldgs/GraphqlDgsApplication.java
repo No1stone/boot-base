0fc70bd992b967ea9a72ea3ddb423fdce9339b42
jangwonseok <jangws1003@naver.com>
2025-11-28 14:44:26 +0900

    [graphql][feat]: build test

--- graphql-dgs/src/main/java/com/origemite/graphqldgs/GraphqlDgsApplication.java
+++ graphql-dgs/src/main/java/com/origemite/graphqldgs/GraphqlDgsApplication.java
@@ -1,9 +1,22 @@
 package com.origemite.graphqldgs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+
+@Slf4j
+@EnableConfigurationProperties
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.origemite.graphqldgs",
+                //시큐리티 부분 의존성걸려서 우선제외
+//                "com.origemite.lib.common",
+//                "com.origemite.lib.model",
+        },
+        exclude = {SecurityAutoConfiguration.class})
 public class GraphqlDgsApplication {
 
     public static void main(String[] args) {
