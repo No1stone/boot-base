751ba28e6ae7922cd7d6bd1e5c1eaff88668ae13
jangwonseok <jangws1003@naver.com>
2025-11-29 20:34:01 +0900

    [graphql][feat]: grpc service 데모코드

---
+++ api-auth/src/main/java/com/origemite/apiauth/member/grpc/MemberIdentificationFacadeGrpc.java
@@ -0,0 +1,42 @@
+package com.origemite.apiauth.member.grpc;
+
+import com.origemite.apiauth.member.facade.MemberIdentificationFacade;
+import com.origemite.apiauth.member.grpc.MemberIdentificationGrpc.MemberIdentificationFindByIdRequest;
+import com.origemite.apiauth.member.grpc.MemberIdentificationGrpc.MemberIdentificationItemResponse;
+import com.origemite.apiauth.member.grpc.MemberIdentificationGrpc.MemberIdentificationServiceGrpc;
+import com.origemite.lib.model.auth.dto.MemberIdentificationRes;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+@Slf4j
+public class MemberIdentificationFacadeGrpc extends MemberIdentificationServiceGrpc.MemberIdentificationServiceImplBase {
+
+    private final MemberIdentificationFacade memberIdentificationFacade;
+
+
+    @Override
+    public void findById(MemberIdentificationFindByIdRequest request,
+                         io.grpc.stub.StreamObserver<com.origemite.apiauth.member.grpc.MemberIdentificationGrpc.MemberIdentificationItemResponse> responseObserver) {
+
+        MemberIdentificationRes.Item item = memberIdentificationFacade.findById(request.getId());
+
+        MemberIdentificationItemResponse response =
+                MemberIdentificationItemResponse.newBuilder()
+                        .setId(item.getId())
+                        .setMemberId(item.getMemberId())
+                        .setName(item.getName())
+                        .setMobilePhoneNumber(item.getMobilePhoneNumber())
+                        .setEmail(item.getEmail())
+                        .setCi(item.getCi())
+                        .setStatus(item.getStatus())
+                        .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
